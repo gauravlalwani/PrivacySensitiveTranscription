0240c6fead1c80aaf5c398d0fa332e87c21fe5b5
Brandon U Dang <budang@saab.ischool.utexas.edu>
2017-11-19 22:08:33 -0600

    updates

--- subjectSetCreator.py
+++ subjectSetCreator.py
@@ -2,66 +2,70 @@
 # This program takes a list of images as inputs (i.e., a file) and bins them into subject sets of size n
 # Code adapted from Mona Mishra
 
-import argparse
-
 from panoptes_client import SubjectSet, Subject, Project, Panoptes
 from pymongo import MongoClient
 
-import db_config
+import argparse
+import itertools
+
+import csh_db_config
 import zooniverse_config
 
-# get filename and subject set size as input from the command line
-ap = argparse.ArgumentParser()
-ap.add_argument('-f', '--filename', required=True, dest='filename',
-                help='The name of the file from which to read the images')
-ap.add_argument('-n', '--size', required=False, dest='n',
-                help='The maximum number of images a subject set should contain. \
-                      The value should be between 1 and 10000, inclusive')
-args = vars(ap.parse_args())
+def main():
+    ap = argparse.ArgumentParser(description='Given a list of images, bins them into subject sets of size n')
 
-filename = args['filename']
-try:
-    n = args['n']
-    assert(n > 1 and n < 10001)
-except:
-    n = 1000
+    # require file path to read in images
+    ap.add_argument('-f', '--filename', required=True, dest='filename', type=str,
+                    help='The name of the file from which to read the images')
 
-# connect to zooniverse
-Panoptes.connect(username=zooniverse_config.Zooniverse_USERNAME,
-                 password=zooniverse_config.Zooniverse_PASS)
+    # optionally require subject set size; defaults to 1000
+    ap.add_argument('-n', '--size', required=False, dest='n', type=int, default=1000,
+                    help='The maximum number of images a subject set should contain. \
+                          The value should be between 1 and 10000, inclusive')
 
-# find the project using project id and subject set id
-project = Project.find(zooniverse_config.Project_ID);
+    # parse args into variables and check values
+    args = vars(ap.parse_args())
 
-# connection to mongodb
-mongoConn = MongoClient(db_config.DB_HOST + ":" + str(db_config.DB_PORT))
-claciTransDB = mongoConn[db_config.TRANSCRIPTION_DB_NAME]
-claciTransDB.authenticate(db_config.TRANSCRIPTION_DB_USER,      #DB authentication
-                          db_config.TRANSCRIPTION_DB_PASS)
+    filename = args['filename']
+    n        = args['n']
 
-# get the image filenames in a Python list
-with open(filename) as handle:
-    filenames = handle.readlines()
+    if not (n >= 1 and n <= 10000):
+        raise ValueError('n must be between 1 and 10000, inclusive')
 
-# divide files into groups of n
-filegroups = list([e for e in t if e != None] for t in itertools.zip_longest(*([iter(filenames)] * n)))
+    # connect to zooniverse
+    Panoptes.connect(username=zooniverse_config.Zooniverse_USERNAME,
+                     password=zooniverse_config.Zooniverse_PASS)
 
-for group in filegroups:
-    # create a new subject set for each file group
-    subjectSet = SubjectSet()
+    # find the project using project id and subject set id
+    project = Project.find(zooniverse_config.Project_ID)
 
-    # create a new subject for each file and add to the subject set
-    for filename in group:
-        subject = Subject()
-        subject.links.project = project
-        fileLocation = zooniverse_config.Image_Folder + filename
-        subject.add_location(fileLocation)
+    # connection to mongodb
+    mongoConn = MongoClient(csh_db_config.DB_HOST + ":" + str(csh_db_config.DB_PORT))
+    cshTransDB = mongoConn[csh_db_config.TRANSCRIPTION_DB_NAME]
+    cshTransDB.authenticate(csh_db_config.TRANSCRIPTION_DB_USER,
+                            csh_db_config.TRANSCRIPTION_DB_PASS)
+    cshCollection = cshTransDB[csh_db_config.TRANS_DB_MeetingMinColl]
+    cshSubjectSets = cshTransDB[csh_db_config.TRANS_DB_SubjectSets]
 
-        # saves locationBasedImageFile and ID as the metadata
-        subject.metadata['ID'] = resultItem["_id"];
-        subject.save()
+    # get the image filenames in a Python list
+    with open(filename) as handle:
+        filenames = handle.readlines()
 
-        # saving the subject in subject set
-        subjectSet.add(subject)
+    # divide files into groups of n
+    filegroups = list([e for e in t if e != None] for t in itertools.zip_longest(*([iter(filenames)] * n)))
 
-        # update file subject set metadata attribute
+    for group in filegroups:
+        # create a new subject for each file and add to the subject set
+        for filename in group:
+            # retrieve, verify, and update the record from mongodb
+            record = cshCollection.find_one_and_update({'_id': filename},
+                                                       {'$set': {'canCrowdsource': True}})
+            if not record:
+                continue
+
+            # update file subject set metadata attribute
+            
+            
+if __name__ == '__main__':
+    main()
+
